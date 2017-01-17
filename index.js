a562b6ac2fcaf8bbbd6f01e9655cc5db7f0c88e0
codyzu <cody.zuschlag@gmail.com>
2017-01-17 10:20:11 +0100

    fix: default parameter handling for API

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jira = require('./jira')
 const parser = require('./parser')
 
-module.exports = function ({username, password, host, baseUrl}) {
+module.exports = function ({username, password, host, baseUrl} = {}) {
   return parser(jira({username, password, host, baseUrl}))
 }
